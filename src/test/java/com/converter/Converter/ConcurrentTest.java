7de6acd51d4c29b2a5bf2e2a98cd0deee0ae477a
Oscar Pinto <moka117@hotmail.com>
2018-12-04 11:21:26 -0500

    entrega

--- src/test/java/com/converter/Converter/ConcurrentTest.java
+++ src/test/java/com/converter/Converter/ConcurrentTest.java
@@ -1,10 +1,18 @@
 package com.converter.Converter;
 
+import com.converter.Converter.Controller.ConverterController;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -13,11 +21,50 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 public class ConcurrentTest {
     @LocalServerPort
     private int port;
+    @MockBean
+    private ConverterController cc;
 
 
     @Test
     public void mustSurviveTenRequests(){
+        ArrayList<Thread> th= new ArrayList<>();
+        for(int i=0;i<10;i++){
+            Thread thread = new Thread(){
+                public void run(){
+                    cc.ConvertCelsiusToFahrenheit(87);
+                    cc.ConvertFahrenheitToCelsius(54);
+                }
+            };
+            th.add(thread);
+
+        }
+        for(int i=0;i<th.size();i++){
+            th.get(i).start();
+        }
+        Assert.assertTrue(true);
 
 
     }
+    @Test
+    public void mustSurviveTenRequests2(){
+        ArrayList<Thread> th= new ArrayList<>();
+        for(int i=0;i<10;i++){
+            Thread thread = new Thread(){
+                public void run(){
+                    MockMvcRequestBuilders.get("/Converter/fahrenheit/82");
+                    MockMvcRequestBuilders.get("/Converter/celsius/82");
+                }
+            };
+            th.add(thread);
+
+        }
+        for(int i=0;i<th.size();i++){
+            th.get(i).start();
+        }
+        Assert.assertTrue(true);
+
+
+    }
+
 }
+
